ac68aaa87e2fb1d68e30ec8a51f8a88ac5bb7341
vijitha mahesh <185044c@uom.lk>
2022-03-01 10:41:01 +0530

    develop: insert data to footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import { SampleBase } from './components/TestWordProcess';
-import './App.css'
-import * as React from 'react';
-import { DocumentEditorContainerComponent, Toolbar, CustomToolbarItemModel } from '@syncfusion/ej2-react-documenteditor';
-
+import { useState } from "react";
+import logo from "./logo.svg";
+import { SampleBase } from "./components/TestWordProcess";
+import "./App.css";
+import * as React from "react";
+import {
+  DocumentEditorContainerComponent,
+  Toolbar,
+  CustomToolbarItemModel,
+} from "@syncfusion/ej2-react-documenteditor";
 
 DocumentEditorContainerComponent.Inject(Toolbar);
 
@@ -16,32 +19,42 @@ export default class App extends SampleBase {
     super(...arguments);
     // Add event listener for requestNavigate event to customize hyperlink navigation functionality
     this.requestNavigate = (args) => {
-        if (args.linkType !== 'Bookmark') {
-            let link = args.navigationLink;
-            if (args.localReference.length > 0) {
-                link += '#' + args.localReference;
-            }
-            //Navigate to the specified URL.
-            window.open(link);
-            args.isHandled = true;
+      if (args.linkType !== "Bookmark") {
+        let link = args.navigationLink;
+        if (args.localReference.length > 0) {
+          link += "#" + args.localReference;
         }
+        //Navigate to the specified URL.
+        window.open(link);
+        args.isHandled = true;
+      }
     };
   }
 
-  render() {
+  public rendereComplete(): void {
+    let document = `{"sections":[{"sectionFormat":{"pageWidth":612,"pageHeight":792,"leftMargin":72,"rightMargin":72,"topMargin":72,"bottomMargin":72,"differentFirstPage":false,"differentOddAndEvenPages":false,"headerDistance":36,"footerDistance":36,"bidi":false},"blocks":[{"paragraphFormat":{"afterSpacing":30,"styleName":"Heading 1","listFormat":{}},"characterFormat":{},"inlines":[{"characterFormat":{}}]}],"headersFooters":{"header":{"blocks":[{"paragraphFormat":{"listFormat":{}},"characterFormat":{},"inlines":[{"text":"Adventure Works Cycles"}]}]},"footer":{"blocks":[{"paragraphFormat":{"listFormat":{}},"characterFormat":{},"inlines":[{"text":"Adventure Works Cycles"}]}]}}}],"characterFormat":{"bold":false,"italic":false,"fontSize":11,"fontFamily":"Calibri","underline":"None","strikethrough":"None","baselineAlignment":"Normal","highlightColor":"NoColor","fontColor":"empty","fontSizeBidi":11,"fontFamilyBidi":"Calibri","allCaps":false},"paragraphFormat":{"leftIndent":0,"rightIndent":0,"firstLineIndent":0,"textAlignment":"Left","beforeSpacing":0,"afterSpacing":0,"lineSpacing":1.0791666507720947,"lineSpacingType":"Multiple","listFormat":{},"bidi":false},"defaultTabWidth":36,"trackChanges":false,"enforcement":false,"hashValue":"","saltValue":"","formatting":false,"protectionType":"NoProtection","dontUseHTMLParagraphAutoSpacing":false,"formFieldShading":true,"styles":[{"name":"Normal","type":"Paragraph","paragraphFormat":{"lineSpacing":1.149999976158142,"lineSpacingType":"Multiple","listFormat":{}},"characterFormat":{"fontFamily":"Calibri"},"next":"Normal"},{"name":"Default Paragraph Font","type":"Character","characterFormat":{}},{"name":"Heading 1 Char","type":"Character","characterFormat":{"fontSize":16,"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Default Paragraph Font"},{"name":"Heading 1","type":"Paragraph","paragraphFormat":{"beforeSpacing":12,"afterSpacing":0,"outlineLevel":"Level1","listFormat":{}},"characterFormat":{"fontSize":16,"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Normal","link":"Heading 1 Char","next":"Normal"},{"name":"Heading 2 Char","type":"Character","characterFormat":{"fontSize":13,"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Default Paragraph Font"},{"name":"Heading 2","type":"Paragraph","paragraphFormat":{"beforeSpacing":2,"afterSpacing":6,"outlineLevel":"Level2","listFormat":{}},"characterFormat":{"fontSize":13,"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Normal","link":"Heading 2 Char","next":"Normal"},{"name":"Heading 3","type":"Paragraph","paragraphFormat":{"leftIndent":0,"rightIndent":0,"firstLineIndent":0,"textAlignment":"Left","beforeSpacing":2,"afterSpacing":0,"lineSpacing":1.0791666507720947,"lineSpacingType":"Multiple","outlineLevel":"Level3","listFormat":{}},"characterFormat":{"fontSize":12,"fontFamily":"Calibri Light","fontColor":"#1F3763"},"basedOn":"Normal","link":"Heading 3 Char","next":"Normal"},{"name":"Heading 3 Char","type":"Character","characterFormat":{"fontSize":12,"fontFamily":"Calibri Light","fontColor":"#1F3763"},"basedOn":"Default Paragraph Font"},{"name":"Heading 4","type":"Paragraph","paragraphFormat":{"leftIndent":0,"rightIndent":0,"firstLineIndent":0,"textAlignment":"Left","beforeSpacing":2,"afterSpacing":0,"lineSpacing":1.0791666507720947,"lineSpacingType":"Multiple","outlineLevel":"Level4","listFormat":{}},"characterFormat":{"italic":true,"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Normal","link":"Heading 4 Char","next":"Normal"},{"name":"Heading 4 Char","type":"Character","characterFormat":{"italic":true,"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Default Paragraph Font"},{"name":"Heading 5","type":"Paragraph","paragraphFormat":{"leftIndent":0,"rightIndent":0,"firstLineIndent":0,"textAlignment":"Left","beforeSpacing":2,"afterSpacing":0,"lineSpacing":1.0791666507720947,"lineSpacingType":"Multiple","outlineLevel":"Level5","listFormat":{}},"characterFormat":{"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Normal","link":"Heading 5 Char","next":"Normal"},{"name":"Heading 5 Char","type":"Character","characterFormat":{"fontFamily":"Calibri Light","fontColor":"#2F5496"},"basedOn":"Default Paragraph Font"},{"name":"Heading 6","type":"Paragraph","paragraphFormat":{"leftIndent":0,"rightIndent":0,"firstLineIndent":0,"textAlignment":"Left","beforeSpacing":2,"afterSpacing":0,"lineSpacing":1.0791666507720947,"lineSpacingType":"Multiple","outlineLevel":"Level6","listFormat":{}},"characterFormat":{"fontFamily":"Calibri Light","fontColor":"#1F3763"},"basedOn":"Normal","link":"Heading 6 Char","next":"Normal"},{"name":"Heading 6 Char","type":"Character","characterFormat":{"fontFamily":"Calibri Light","fontColor":"#1F3763"},"basedOn":"Default Paragraph Font"}],"lists":[],"abstractLists":[],"comments":[],"revisions":[],"customXml":[]}`;
+    this.container?.documentEditor.open(document);
+    this.container?.documentEditor.selection.goToFooter();
+    this.container?.documentEditor.editor.insertImage(
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png",
+      60,
+      60
+      );
+  }
 
+  render() {
     let toolItem: CustomToolbarItemModel = {
       prefixIcon: "e-de-ctnr-lock",
       tooltipText: "Disable Image",
       text: "Disable Image",
-      id: "Custom"
+      id: "Custom",
     };
 
     let items = [
       toolItem,
-      'New',
-      'Open',
-      'Separator',
+      "New",
+      "Open",
+      "Separator",
       "Undo",
       "Redo",
       "Separator",
@@ -61,22 +74,20 @@ export default class App extends SampleBase {
       "Find",
       "Separator",
       "LocalClipboard",
-      "RestrictEditing"
+      "RestrictEditing",
     ];
 
     return (
       <DocumentEditorContainerComponent
-        ref={scope => {
+        ref={(scope) => {
           this.container = scope;
         }}
         id="container"
-        height={'700px'}
+        height={"700px"}
         toolbarItems={items}
         toolbarClick={this.onToolbarClick.bind(this)}
         enableToolbar={true}
-
       />
-
     );
   }
 
@@ -90,5 +101,4 @@ export default class App extends SampleBase {
         break;
     }
   };
-
 }
