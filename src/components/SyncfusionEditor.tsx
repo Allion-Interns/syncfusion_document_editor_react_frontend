6ba5641ec6305a07067313b5f28e4125628528d4
vijitha mahesh <185044c@uom.lk>
2022-03-02 20:35:24 +0530

    bug fix: save function

--- src/components/SyncfusionEditor.tsx
+++ src/components/SyncfusionEditor.tsx
@@ -1,9 +1,9 @@
 import { SampleBase } from './TestWordProcess';
-import { DocumentEditorContainerComponent, Toolbar, CustomToolbarItemModel } from '@syncfusion/ej2-react-documenteditor';
+import { DocumentEditorContainerComponent, Toolbar, CustomToolbarItemModel, SfdtExport, DocumentEditorKeyDownEventArgs } from '@syncfusion/ej2-react-documenteditor';
 import { InitialDocumentTemplate } from '../templates/InitialDocument';
 
 
-DocumentEditorContainerComponent.Inject(Toolbar);
+DocumentEditorContainerComponent.Inject(Toolbar,SfdtExport);
 
 export default class SyncfusionEditor extends SampleBase {
   constructor(props: any) {
@@ -14,6 +14,7 @@ export default class SyncfusionEditor extends SampleBase {
 
     };
   }
+
 
   public container: DocumentEditorContainerComponent = new DocumentEditorContainerComponent({});
 
@@ -29,11 +30,44 @@ export default class SyncfusionEditor extends SampleBase {
     this.container.documentEditor.selection.select(startOffset, startOffset);
   }
 
-  public async rendereComplete(): Promise<void> {
+// public overrideSaveFunction(a:DocumentEditorContainerComponent):void{
+//   this.container.documentEditor.keyDown = async function (args: DocumentEditorKeyDownEventArgs) {
+//     let keyCode: number = args.event.which || args.event.keyCode;
 
+//     let isCtrlKey: boolean = (args.event.ctrlKey || args.event.metaKey) ? true : ((keyCode === 17) ? true : false);
+
+//     let isAltKey: boolean = args.event.altKey ? args.event.altKey : ((keyCode === 18) ? true : false);
+
+//    //83 is the character code for 'S'
+//     if (isCtrlKey && !isAltKey && keyCode === 83) {
+//         //To prevent default save operation, set the isHandled property to true
+//         let as  = await a.container.documentEditor.serialize();
+//         let obj1 = JSON.parse(a);
+//         obj1.sections[0].headersFooters.header.blocks[0].rows[0].cells[0].blocks[0].inlines[0].text = 'row 1 column 1'; 
+//         obj1.sections[0].headersFooters.header.blocks[0].rows[0].cells[1].blocks[0].inlines[0].text = 'row 1 column 2';
+//         obj1.sections[0].headersFooters.header.blocks[0].rows[0].cells[2].blocks[0].inlines[0].text = 'row 1 column 3';
+//         obj1.sections[0].headersFooters.header.blocks[0].rows[1].cells[0].blocks[0].inlines[0].text = 'row 2 column 1';
+//         obj1.sections[0].headersFooters.header.blocks[0].rows[1].cells[1].blocks[0].inlines[0].text = 'row 2 column 2';
+//         obj1.sections[0].headersFooters.header.blocks[0].rows[1].cells[2].blocks[0].inlines[0].text = 'row 2 column 3';
+   
+   
+//         obj1.sections[0].headersFooters.footer.blocks[0].rows[1].cells[2].blocks[0].inlines[0].text = 'footer row 2 column 3';
+//         let s = JSON.stringify(obj1);
+//        a.setState({editedDoc:s});
+//        a.container.documentEditor.open(this.state.editedDoc);
+   
+//        a.container.documentEditor.save('sample', 'Sfdt');
+//     } 
+//   };
+
+// }
+
+  public async rendereComplete(): Promise<void> {
+   // this.overrideSaveFunction(this.container);
     let initialDoc = InitialDocumentTemplate;
     await this.setState({ initialDoc: initialDoc});
     await this.container.documentEditor.open(initialDoc);
+
   }
 
   render() {
@@ -74,8 +108,7 @@ export default class SyncfusionEditor extends SampleBase {
 
     return (
       <div>
-        <button onClick={this.buttonOnclick} >save </button>
-
+        <button id='export' onClick={this.buttonOnclick} >save </button>
         <DocumentEditorContainerComponent
           ref={scope => {
             this.container = scope;
@@ -85,14 +118,13 @@ export default class SyncfusionEditor extends SampleBase {
           toolbarItems={items}
           toolbarClick={this.onToolbarClick.bind(this)}
           enableToolbar={true}
-
         />
       </div>
-
     );
   }
 
-  onToolbarClick = (args: ClickEventArgs): void => {
+  
+  onToolbarClick = (args: any): void => {
     switch (args.item.id) {
       case "Custom":
         //Disable image toolbar item.
@@ -120,8 +152,7 @@ export default class SyncfusionEditor extends SampleBase {
     this.setState({editedDoc:s});
     this.container.documentEditor.open(this.state.editedDoc);
 
+    this.container.documentEditor.save('sample', 'Sfdt');
   }
-
-
-
+  
 }
