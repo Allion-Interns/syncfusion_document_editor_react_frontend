1eff85fb3ee7722568f2cc01902b15c3a92c47d2
pasindu-osadha <pasiduosanda@gmail.com>
2022-03-04 21:54:25 +0530

    integration attempt01

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
-
 import './App.css';
+import { SampleBase } from './components/SampleBase';
 import SyncfusionEditor from './components/SyncfusionEditor';
 
-function App() {
-  return (
-    <div>
-    <SyncfusionEditor/>
-    </div>
-  );
+export default class App extends SampleBase {
+ 
+  constructor() {
+    super(...arguments);
+    this.state={
+       setlocal:false,
+       setlanguage:false
+    }
+
+  }
+
+  public rendereComplete(): void {
+  
+  }
+  
+
+  render() {
+
+    if(!this.state.setlanguage){
+      return(
+        <div>
+          
+          <button onClick={this.languagechangeClickSwedesh.bind(this)} >Swedesh</button>
+          <button onClick={this.languagechangeClickEnglish.bind(this)} >English</button>
+
+        </div>
+      )
+    }
+    
+    return (
+      <div>
+        
+        <SyncfusionEditor language={this.state.setlocal}/>  
+      </div>
+
+    );
+  }
+
+
+languagechangeClickSwedesh() {
+  this.setState({setlocal:true});
+  this.setState({setlanguage:true});
+}
+languagechangeClickEnglish() {
+  this.setState({setlocal:false});
+  this.setState({setlanguage:true});
 }
 
-export default App;
+}
